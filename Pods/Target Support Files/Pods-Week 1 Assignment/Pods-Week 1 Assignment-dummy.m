d38925d2961c4ffe1ba84631afe568f4b927e553
mkinoshita22 <mkinoshita@csumb.edu>
2018-07-20 17:14:00 -0700

    Week1TPm

---
+++ Pods/Target Support Files/Pods-Week 1 Assignment/Pods-Week 1 Assignment-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Week_1_Assignment : NSObject
+@end
+@implementation PodsDummy_Pods_Week_1_Assignment
+@end
---
+++ Week 1 Assignment/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  Week 1 Assignment
+//
+//  Created by Mark Kinoshita on 7/20/18.
+//  Copyright © 2018 Mark Kinoshita. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
